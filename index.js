0fcde08fa954a98f5e7464cbcb0a6fc3723aad7a
Wolf Hoffman <michaelhoffmanj@gmail.com>
2018-05-16 18:23:24 -0600

    init

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var Yanny = function () {
-    this.isYanny = function () {
-        return true;
-    }
-
-    this.isLaurel = function () {
-        return false;
-    }
-}
-
-var Laurel = function () {
-    this.isYanny = function () {
-        return false;
-    }
-
-    this.isLaurel = function () {
-        return true;
-    }
-}
-
 module.exports = {
-    Yanny,
-    Laurel
-};
+
+    
+     Yanny: function() {
+        this.isYanny = function () {
+            return true;
+        }
+        
+        this.isLaurel = function () {
+            return false;
+        }
+    },
+    
+    Laurel: function() {
+        this.isYanny = function () {
+            return false;
+        }
+        
+        this.isLaurel = function () {
+            return true;
+        }
+    }
+    
+}
--- test.js
+++
@@ -1,4 +0,0 @@
-var laurel = require('./index').Laurel;
-var yanny = require('./index').Yanny;
-
-console.log(yanny.isYanny());
